c25d7e1747bec49b20396c3775e43785b0e2d935
Nikita Korshunov <kewka96@gmail.com>
2019-04-07 01:08:35 +0500

    Add setupTests

--- src/lib/YandexApi/YandexApi.test.ts
+++ src/lib/YandexApi/YandexApi.test.ts
@@ -1,7 +1,22 @@
-import YandexApi from './YandexApi';
+import YandexApi, { YandexApiOptions } from './YandexApi';
 
 describe('YandexApi', () => {
   it('to be truthy', () => {
     expect(YandexApi).toBeTruthy();
   });
+
+  it('load the ymaps instance', async () => {
+    const options: YandexApiOptions = {
+      lang: 'en_RU',
+      force: true,
+    };
+
+    const ymaps = await YandexApi.load(options);
+
+    expect(ymaps).toBeTruthy();
+    expect(ymaps.meta.languageCode).toBe('en');
+    expect(ymaps.meta.countryCode).toBe('RU');
+  });
+
+  // TODO: add more tests.
 });
---
+++ src/setupTests.ts
@@ -0,0 +1,72 @@
+import { JSDOM } from 'jsdom';
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>', {
+  runScripts: 'dangerously',
+  resources: 'usable',
+  url: 'https://react.ymaps',
+  referrer: 'https://react.ymaps',
+});
+
+const { window } = jsdom;
+
+function copyProps(src, target) {
+  Object.defineProperties(target, {
+    ...Object.getOwnPropertyDescriptors(src),
+    ...Object.getOwnPropertyDescriptors(target),
+  });
+}
+
+global.window = window;
+global.document = window.document;
+global.requestAnimationFrame = function(callback) {
+  return setTimeout(callback, 0);
+};
+global.cancelAnimationFrame = function(id) {
+  clearTimeout(id);
+};
+
+//
+// Mock Canvas / Context2D calls
+//
+window.HTMLCanvasElement.prototype.getContext = function() {
+  return {
+    fillRect() {},
+    clearRect() {},
+    getImageData(x, y, w, h) {
+      return {
+        data: new Array(w * h * 4),
+      };
+    },
+    putImageData() {},
+    createImageData() {
+      return [];
+    },
+    setTransform() {},
+    drawImage() {},
+    save() {},
+    fillText() {},
+    restore() {},
+    beginPath() {},
+    moveTo() {},
+    lineTo() {},
+    closePath() {},
+    stroke() {},
+    translate() {},
+    scale() {},
+    rotate() {},
+    arc() {},
+    fill() {},
+    measureText() {
+      return { width: 0 };
+    },
+    transform() {},
+    rect() {},
+    clip() {},
+  };
+};
+
+window.HTMLCanvasElement.prototype.toDataURL = function() {
+  return '';
+};
+
+copyProps(window, global);
